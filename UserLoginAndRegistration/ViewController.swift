9fa39ea477b6972d2aaf162c9530476f1de2c89c
Sergey Kargopolov <sergeykargopolov@Sergeys-MacBook-Pro.local>
2015-01-08 20:50:59 -0800

    Initial Commit

---
+++ UserLoginAndRegistration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserLoginAndRegistration
+//
+//  Created by Sergey Kargopolov on 2015-01-08.
+//  Copyright (c) 2015 Sergey Kargopolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
